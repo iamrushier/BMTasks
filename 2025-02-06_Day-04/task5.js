2c0afea51ad1489b69b18a2a57747e24acf87541
iamrushier <rushikeshsurve193@gmail.com>
2025-02-06 13:44:23 +0530

    Task 5: Callback functions with map(),filter(),reduce() complete.

--- 2025-02-06_Day-04/task5.js
+++ 2025-02-06_Day-04/task5.js
@@ -5,3 +5,21 @@
 // * If the callback is doubleNumbers, double each number.
 // * If the callback is calculateSum, return the sum of all numbers.
 // Bonus Task: Implement a callback to find the maximum number in the array.
+
+function processData(arr, func) {
+  return func(arr);
+}
+const filterOdd = (arr) => arr.filter((num) => num % 2 !== 0);
+
+const doubleNumbers = (arr) => arr.map((num) => num * 2);
+
+const calculateSum = (arr) => arr.reduce((prev, curr) => prev + curr);
+
+const getMax = (arr) => arr.reduce((prev, curr) => (prev > curr ? prev : curr));
+
+arr = [65, 34, 1, 34, 5];
+console.log("Original array:", arr);
+console.log("Odd numbers filtered:", processData(arr, filterOdd));
+console.log("Doubled numbers:", processData(arr, doubleNumbers));
+console.log("Sum of numbers:", processData(arr, calculateSum));
+console.log("Maximum of numbers:", processData(arr, getMax));
