5a2ab36950f5facd2f9e306c2076da51dd283029
iamrushier <rushikeshsurve193@gmail.com>
2025-02-06 15:55:25 +0530

    Task 2: Working with objects

--- 2025-02-06_Day-04/task2.js
+++ 2025-02-06_Day-04/task2.js
@@ -6,3 +6,56 @@
 //     * getBooksByAuthor(author): Returns all books by a given author.
 //     * removeBook(title): Removes a book by title.
 //     * Add a method getAllBooks to return a list of all book titles.
+
+const bookLibrary = {
+  books: [
+    {
+      title: "Programming Python",
+      author: "Mark",
+      yearPublished: 2007,
+    },
+    {
+      title: "The Discovery of India",
+      author: "Nehru",
+      yearPublished: 1946,
+    },
+    {
+      title: "Wings of Fire",
+      author: "Kalam",
+      yearPublished: 1999,
+    },
+    {
+      title: "Let us C",
+      author: "Kanetkar",
+      yearPublished: 1999,
+    },
+    {
+      title: "Let us Python",
+      author: "Kanetkar",
+      yearPublished: 2005,
+    },
+  ],
+  addBook: function (book) {
+    this.books.push(book);
+  },
+  getBooksByAuthor: function (author) {
+    return this.books.filter((book) => book.author === author);
+  },
+  removeBook: function (title) {
+    this.books = this.books.filter((book) => book.title !== title);
+  },
+  getAllBooks: function () {
+    return this.books.map((book) => book.title);
+  },
+};
+// console.log(bookLibrary.books);
+console.log(bookLibrary.getAllBooks());
+console.log(bookLibrary.getBooksByAuthor("Kanetkar"));
+bookLibrary.removeBook("Let us C");
+console.log(bookLibrary.getAllBooks());
+bookLibrary.addBook({
+  title: "Let us C",
+  author: "Kanetkar",
+  yearPublished: 1999,
+});
+console.log(bookLibrary.getBooksByAuthor("Kanetkar"));
