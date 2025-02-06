f3693b89c0615e24c7ab970b53dbec31fd9629ac
iamrushier <rushikeshsurve193@gmail.com>
2025-02-06 17:53:51 +0530

    Task 1: Callback functions

--- 2025-02-06_Day-04/task1.js
+++ 2025-02-06_Day-04/task1.js
@@ -1,5 +1,5 @@
 // Task 1: Callback Functions
-//    * Write a function fetchData that simulates fetching data
+//    *- Write a function fetchData that simulates fetching data
 //          from a server using a callback function.
 //    * The function should take a callback that processes the data
 //          after a delay of 2 seconds.
@@ -7,3 +7,19 @@
 //    * The data should be an array of user names.
 // Bonus Task: Implement error handling in the callback function
 //      to simulate a case where the server might fail.
+function fetchData(func) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (Math.random() <= 0.5)
+        resolve(["ramesh", "suresh", "amar", "akbar", "anthony"]);
+      else reject("Rejected");
+    }, 2000);
+  })
+    .then((res) => {
+      console.log(func(res));
+    })
+    .catch((err) => console.log("Error", err));
+}
+const toUpper = (arr) => arr.map((name) => name.toUpperCase());
+
+fetchData(toUpper);
