df3b1e1b2a6c49e8c01e9fab35dec7d75377da81
iamrushier <rushikeshsurve193@gmail.com>
2025-02-13 21:06:15 +0530

    Fixed bug in storage.ts

--- 2025-02-13_Day-09/ExpenseTracker/src/storage.ts
+++ 2025-02-13_Day-09/ExpenseTracker/src/storage.ts
@@ -2,7 +2,7 @@ function saveToLocalStorage<T, U>(key: T, data: U): void {
   localStorage.setItem(JSON.stringify(key), JSON.stringify(data));
 }
 function getFromLocalStorage<T, U>(key: T): U {
-  const serializedData = localStorage.getItem(JSON.stringify(key)) || "";
+  const serializedData = localStorage.getItem(JSON.stringify(key)) || "[]";
   return JSON.parse(serializedData) as U;
 }
 export { saveToLocalStorage, getFromLocalStorage };
