f06f3d869005490dee09f2e28118cb794a57eca5
iamrushier <rushikeshsurve193@gmail.com>
2025-02-13 21:08:08 +0530

    Removed commented code from eventHandler.ts

--- 2025-02-13_Day-09/ExpenseTracker/src/eventHandlers.ts
+++ 2025-02-13_Day-09/ExpenseTracker/src/eventHandlers.ts
@@ -3,45 +3,6 @@ import { addNewExpense, filterExpenses } from "./expenseManager";
 import { Expense } from "./types";
 import { extractNewExpense, renderExpensesData } from "./ui";
 
-// const expenseTitleInput: HTMLInputElement = document.querySelector(
-//   "#expense-title"
-// ) as HTMLInputElement;
-// const expenseCostInput: HTMLInputElement = document.querySelector(
-//   "#expense-cost"
-// ) as HTMLInputElement;
-// const expenseCategoryInput: HTMLInputElement = document.querySelector(
-//   "#expense-category"
-// ) as HTMLInputElement;
-// const expenseDescriptionInput: HTMLInputElement = document.querySelector(
-//   "#expense-description"
-// ) as HTMLInputElement;
-// const expenseDateInput: HTMLInputElement = document.querySelector(
-//   "#expense-date"
-// ) as HTMLInputElement;
-
-// const addNewButton: HTMLButtonElement = document.querySelector(
-//   "#add-new-expense"
-// ) as HTMLButtonElement;
-
-// function addNewExpense() {
-//   const expenseTitle: string = expenseTitleInput.value;
-//   const expenseCost: number = Number(expenseCostInput.value);
-//   const expenseCategory: string = expenseCategoryInput.value;
-//   const expenseDescription: string = expenseDescriptionInput.value || "";
-//   const expenseDate: Date = new Date(expenseDateInput.value);
-
-//   const newExpense: Expense = {
-//     title: expenseTitle,
-//     cost: expenseCost,
-//     category: expenseCategory,
-//     description: expenseDescription,
-//     date: expenseDate,
-//   };
-//   console.log(newExpense);
-// }
-
-// export default addNewExpense;
-
 function addExpenseCallback(event: Event): void {
   const newExpense: Expense = extractNewExpense();
   const expensesData: Expense[] = addNewExpense(newExpense);
