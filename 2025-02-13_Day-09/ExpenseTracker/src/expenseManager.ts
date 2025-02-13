18fd0b03327000b2a01bef721466c8975b29186b
iamrushier <rushikeshsurve193@gmail.com>
2025-02-13 19:50:05 +0530

    Added functions for operations with expense data

---
+++ 2025-02-13_Day-09/ExpenseTracker/src/expenseManager.ts
@@ -0,0 +1,48 @@
+import { getFromLocalStorage, saveToLocalStorage } from "./storage";
+import { Expense, expenseTrackerKey } from "./types";
+
+function addNewExpense(expense: Expense): void {
+  const oldExpensesData: Expense[] = getFromLocalStorage<string, Expense[]>(
+    expenseTrackerKey
+  );
+  oldExpensesData.push(expense);
+  const newExpensesData: Expense[] = structuredClone(oldExpensesData);
+  saveToLocalStorage(expenseTrackerKey, newExpensesData);
+}
+function filterExpenses(
+  category: string,
+  fromDate: string,
+  untilDate: string
+): Expense[] {
+  const expensesData: Expense[] = getFromLocalStorage<string, Expense[]>(
+    expenseTrackerKey
+  );
+  let findFromDate: Date;
+  let findUntilDate: Date;
+  if (!fromDate) {
+    findFromDate = expensesData.reduce((accum: Date, expense: Expense) => {
+      const date: Date = new Date(expense.date);
+      if (accum < date) return date;
+      else return accum;
+    }, new Date());
+  } else {
+    findFromDate = new Date(fromDate);
+  }
+  if (!untilDate) {
+    findUntilDate = new Date();
+  } else {
+    findUntilDate = new Date(fromDate);
+  }
+  return structuredClone(
+    expensesData
+      .filter(
+        (expense) =>
+          category.toLowerCase() === "all" || category === expense.category
+      )
+      .filter((expense) => {
+        const expenseDate: Date = new Date(expense.date);
+        return findFromDate <= expenseDate && expenseDate <= findUntilDate;
+      })
+  );
+}
+export { addNewExpense, filterExpenses };
