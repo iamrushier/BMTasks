97ed9747f6e264b1b1218889a8950a2b95e5c3fd
iamrushier <rushikeshsurve193@gmail.com>
2025-02-19 11:51:56 +0530

    Initialized React-Posts project

---
+++ 2025-02-19_Day-13/README.md
@@ -0,0 +1,4 @@
+Assignment:
+
+Use https://jsonplaceholder.typicode.com/ sample posts API.
+Implement API HTTP requests in React
---
+++ 2025-02-19_Day-13/react-posts-api/src/App.tsx
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <h1 className="text-center">Posts display will go here</h1>
+    </>
+  );
+}
+
+export default App;
